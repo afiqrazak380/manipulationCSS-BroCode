d93d3c26127a5eac68fe43a7caa146c097d24d4c
Afiq Razak <120654244+afiqrazak380@users.noreply.github.com>
2023-10-21 14:28:51 +0100

    Add files via upload

---
+++ manipulationCSS-BroCode.js
@@ -0,0 +1,10 @@
+// How to change CSS property of Element
+
+const myTitle = document.getElementById("title");
+
+myTitle.style.color = "rgb(90,90,90)";
+myTitle.style.backgroundColor = "lightblue";
+myTitle.style.fontFamily = "sans serif";
+myTitle.style.textAlign = "center";
+myTitle.style.border = "2px solid";
+// myTitle.style.display = "none" // to hide element
